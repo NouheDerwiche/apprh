fc71f97741ad5cfe793535b4cce2975782205287
oumaima bennoureddine <obnr1096@gmail.com>
2023-05-03 11:51:15 +0100

    hoki

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -887,7 +887,7 @@ License: For each use you must have a valid license purchased only from above li
                                                 <div class="symbol symbol-50px me-5">
                                                     <img src="{{ asset('storage/assets/media/avatars/' . Auth::user()->avatar) }}" >                                                </div>
                                                 <!--end::Avatar-->
-{{-- Btest --}}
+
                                                 <!--begin::Username-->
                                                 <div class="d-flex flex-column">
                                                     <div class="fw-bold d-flex align-items-center fs-5">
@@ -2284,31 +2284,7 @@ License: For each use you must have a valid license purchased only from above li
     <!--end::Help drawer-->
     <!--end::Engage drawers-->
 
-    <!--begin::Engage toolbar-->
-    <div
-        class="engage-toolbar d-flex position-fixed px-5 fw-bold zindex-2 top-50 end-0 transform-90 mt-5 mt-lg-20 gap-2">
 
-
-
-
-        <!--begin::Help drawer toggle-->
-        <button id="kt_help_toggle" class="engage-help-toggle btn engage-btn shadow-sm px-5 rounded-top-0"
-            title="Learn & Get Inspired" data-bs-toggle="tooltip" data-bs-custom-class="tooltip-inverse"
-            data-bs-placement="left" data-bs-dismiss="click" data-bs-trigger="hover">
-            Help
-        </button>
-        <!--end::Help drawer toggle-->
-
-        <!--begin::Purchase link-->
-        <a href="https://themes.getbootstrap.com/product/craft-bootstrap-5-admin-dashboard-theme" target="_blank"
-            class="engage-purchase-link btn engage-btn px-5 shadow-sm rounded-top-0">
-            Buy now
-        </a>
-        <!--end::Purchase link-->
-
-
-    </div>
-    <!--end::Engage toolbar-->
     <!--begin::Scrolltop-->
     <div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
         <!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
